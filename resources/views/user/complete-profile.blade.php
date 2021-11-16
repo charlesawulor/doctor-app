af0578df1b9e41740921ae513c33558db27d7b55
Charles Awulor <37975027+charlesawulor@users.noreply.github.com>
2021-11-16 10:54:49 +0100

    admin dashboard complete profile updated

--- resources/views/user/complete-profile.blade.php
+++ resources/views/user/complete-profile.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app1')
 
 @section('content')
-<div class="container">
+
+<div class="container" style="margin-top:150px;margin-bottom:100px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Complete Profile') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -23,7 +24,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> <br>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
@@ -37,7 +38,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> <br>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
@@ -51,7 +52,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> <br>
 
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
@@ -59,12 +60,12 @@
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
-                        </div>
+                        </div> <br>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Submit Profile') }}
                                 </button>
                             </div>
                         </div>
