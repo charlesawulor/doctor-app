96e7ca390ea8e0c3a6b678447938193144c42ecd
Charles Awulor <37975027+charlesawulor@users.noreply.github.com>
2021-11-16 10:38:05 +0100

    admin dashboard complete profile updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/user';
+    protected $redirectTo = '/user/complete-profile';
 
     /**
      * Create a new controller instance.
