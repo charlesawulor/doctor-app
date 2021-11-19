92f298b64602cb31761c58f8c96430fcbbda1b9c
Charles Awulor <37975027+charlesawulor@users.noreply.github.com>
2021-11-19 08:14:50 +0100

    profile completion page modified

--- resources/views/user/complete-profile.blade.php
+++ resources/views/user/complete-profile.blade.php
@@ -87,7 +87,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('About') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <textarea id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"></textarea>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
