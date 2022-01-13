6f48d654984048c857a05785b9b8ace5009f40ce
Charles Awulor <37975027+charlesawulor@users.noreply.github.com>
2022-01-13 15:33:14 +0100

    home

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -167,7 +167,7 @@
                             <span class="badge badge-pill badge-soft-primary">About Doctris</span>
                             <h4 class="title mt-3 mb-4">Good Services And Better <br> Health By Our Certified And Trusted Specialists</h4>
                             <p class="para-desc text-muted">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
-                            <p class="para-desc text-muted">The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin language which more or less corresponds to 'proper' Latin. It contains a series of real Latin words.</p>
+                            <p class="para-desc text-muted">The most well-known telemedicine platform</p>
                             <div class="mt-4">
                                 <a href="aboutus.html" class="btn btn-soft-primary">Read More</a>
                             </div>
